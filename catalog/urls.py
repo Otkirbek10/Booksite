21d0530cfaabc992186b24fef9c1a84c325f1583
O'tkirbek Alimov <99404246+Otkirbek10@users.noreply.github.com>
2024-02-26 23:38:50 +0500

    Added great features

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,50 @@
 from django.urls import path,re_path
-from .views import index
+from .views import *
 
 urlpatterns = [
-    path('',index,name='index')
+    path('',index,name='index'),
+    re_path('^books/$',books_list, name="books"),
+    path('book/<slug:book_slug>/',book_detail,name="book_detail"),
+    path("authors/author/<slug:slug>/",book_author,name='author')
+
 ]
+
+
+
+
+
+# {% extends 'base.html' %}
+
+
+# {% block content %}
+#     {% if book.photo %}
+#         <img src="{{ book.photo.url }}" alt="Ok">
+#     {% endif %}
+#     <h1>Title of the book: {{ book.title }}</h1>
+#     <p><strong>Genre:</strong> {{ book.genre }}</p>
+#     <p><strong>Description:</strong> {{ book.description }}</p>
+#     <p><strong>ISBN:</strong> {{ book.isbn }}</p>
+#     <p><strong>Language:</strong> {{ book.language }}</p>
+#         {% for author in book.author.all %}     
+#             <p><strong>Author:</strong>
+#                 <a href="">{{ author.full_name }}</a></p>
+#         {% endfor %}
+
+# <div style="margin-left: 20px;margin-top: 20px;">
+#     <h4>Amount of the copies</h4>
+#     {% for copy in book.book_instance_set.all %}
+#         <hr><p class="{% if copy.status == 1 %} text-succes
+#                          {% elif copy.status == 2 %} text-danger
+#                          {% else %} text-warning
+#                          {% endif %}">{{copy.get_status_display}}</p>
+            
+#         <p><strong>Pulishing house</strong>{{ copy.imprint }}</p>
+#         <p class="text-muted"><strong>Inventory number</strong>{{ copy.id }}</p>
+#         <p><strong>Status of copy</strong>{{ copy.status }}</p>
+
+#     {% endfor %}
+# </div>
+
+# {% endblock %}
+# <p><strong>Author:</strong><a href="">{{ author.full_name }}</a></p>
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from .models import *
+from django.views import generic
 
 def index(request):
     books = Book.objects.all().count()
     authors = Author.objects.all().count()
-    instances = BookInstance.objects.all().count()
-    return render(request,'catalog/index.html',context={'books':books,'authors':authors,'instances':instances})
+    return render(request,'catalog/index.html',context={'books':books,'authors':authors})
 
 
+def books_list(request): 
+    genres = Genre.objects.all()
+    books = Book.objects.all()
+    return render(request, "catalog/books.html", {'genres': genres, 'books': books}) 
+
+
+def book_detail(request, book_slug):
+    book = get_object_or_404(Book, book_slug=book_slug)
+    return render(request,'catalog/book_detail.html', {'book': book})
+
+def book_author(request,slug):
+    author = get_object_or_404(Author,slug=slug)
+    books = Book.objects.filter(author=author)
+    return render(request,'catalog/author.html',{'author':author,'books':books})
+    
